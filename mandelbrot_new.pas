2dab908fe1c36f9994dcbf7408780eba99ffb2ea
Akira13641 <operator97@gmail.com>
2021-03-17 15:41:05 -0400

    Update mandelbrot_new.pas

--- mandelbrot_new.pas
+++ mandelbrot_new.pas
@@ -25,15 +25,13 @@ const
   Size: PtrInt = 200;
 
 type
-  TDoublePair = record
-    I, R: Double;
-  end;
+  TDoublePair = array[0..1] of Double;
   PDoublePair = ^TDoublePair;
+  
   TData = record
     Rows: PByte;
-    //InitialIR: PDoublePair;
+    InitialIR: PDoublePair;
   end;
-
   PData = ^TData;
 
 var
@@ -48,56 +46,58 @@ var
     XByte, I, J, X: PtrInt;
     ZRA, ZRB, ZIA, ZIB, TRA, TRB, TIA, TIB, CRA, CRB, CI: Double;
   begin
-    CI := Double(Inv * Double(Index)) - 1.0;
-    for XByte := Pred(BytesPerRow) downto 0 do begin
-      Res := 0;
-      I := 0;
-      repeat
-        X := XByte shl 3;
-        CRA := Double(Inv * Double(X + I)) - 1.5;
-        CRB := Double(Inv * Double(X + I + 1)) - 1.5;
-        ZRA := CRA;
-        ZIA := CI;
-        ZRB := CRB;
-        ZIB := CI;
-        B := 0;
-        for J := 49 downto 0 do begin
-          TRA := ZRA * ZRA;
-          TIA := ZIA * ZIA;
-          if TRA + TIA > Limit then begin
-            B := B or 2;
-            if B = 3 then Break;
+    with TData(UserData^) do begin
+      CI := InitialIR[Index][0];
+      for XByte := Pred(BytesPerRow) downto 0 do begin
+        Res := 0;
+        I := 0;
+        repeat
+          X := XByte shl 3;
+          CRA := InitialIR[X + I][1];
+          CRB := InitialIR[X + I + 1][1];
+          ZRA := CRA;
+          ZIA := CI;
+          ZRB := CRB;
+          ZIB := CI;
+          B := 0;
+          for J := 49 downto 0 do begin
+            TRA := ZRA * ZRA;
+            TIA := ZIA * ZIA;
+            if TRA + TIA > Limit then begin
+              B := B or 2;
+              if B = 3 then Break;
+            end;
+            TRB := ZRB * ZRB;
+            TIB := ZIB * ZIB;
+            if TRB + TIB > Limit then begin
+              B := B or 1;
+              if B = 3 then Break;
+            end;
+            ZIA := 2 * ZRA * ZIA + CI;
+            ZRA := TRA - TIA + CRA;
+            ZIB := 2 * ZRB * ZIB + CI;
+            ZRB := TRB - TIB + CRB;
           end;
-          TRB := ZRB * ZRB;
-          TIB := ZIB * ZIB;
-          if TRB + TIB > Limit then begin
-            B := B or 1;
-            if B = 3 then Break;
-          end;
-          ZIA := 2 * ZRA * ZIA + CI;
-          ZRA := TRA - TIA + CRA;
-          ZIB := 2 * ZRB * ZIB + CI;
-          ZRB := TRB - TIB + CRB;
-        end;
-        Res := (Res shl 2) or B;
-        I += 2;
-      until I = 8;
-      TData(UserData^).Rows[(Index * BytesPerRow) + XByte] := not Res;
+          Res := (Res shl 2) or B;
+          I += 2;
+        until I = 8;
+        Rows[(Index * BytesPerRow) + XByte] := not Res;
+      end;
     end;
   end;
-  
-  {procedure MakeLookupTables(Index: PtrInt;
+
+  procedure MakeLookupTables(Index: PtrInt;
                              UserData: Pointer;
                              Item: TMultiThreadProcItem);
   var InvScaled: Double;
   begin
     InvScaled := Inv * Double(Index);
-    with TData(UserData^).InitialIR[Index] do begin
-      I := InvScaled - 1.0;
-      R := InvScaled - 1.5;
+    with TData(UserData^) do begin
+      InitialIR[Index][0] := InvScaled - 1.0;
+      InitialIR[Index][1] := InvScaled - 1.5;
     end;
-  end;}
-  
+  end;
+
 var
   Data: TData;
   {$ifndef Unix}
@@ -109,18 +109,17 @@ begin
   if ParamCount > 0 then Val(ParamStr(1), Size);
   BytesPerRow := Size shr 3;
   with Data do begin
-    //GetMem(InitialIR, SizeOf(TDoublePair) * Size);
+    GetMem(InitialIR, SizeOf(TDoublePair) * Size);
     GetMem(Rows, BytesPerRow * Size);
   end;
   Inv := 2.0 / Double(Size);
   with ProcThreadPool do begin
-    //DoParallel(@MakeLookupTables, 0, Pred(Size), @Data);
+    DoParallel(@MakeLookupTables, 0, Pred(Size), @Data);
     DoParallel(@RenderRows, 0, Pred(Size), @Data);
   end;
   {$ifdef Unix}
     PrintF('P4'#10'%d %d'#10, Size, Size);
-    FWrite(@Data.Rows[0], 1, BytesPerRow * Size, CStdOut);
-    PrintF(#10);
+    FWrite(@Data.Rows[0], BytesPerRow, Size, CStdOut);
   {$else}
     IO := @Output;
     Write(IO^, 'P4', #10, Size, ' ', Size, #10);
@@ -128,7 +127,7 @@ begin
     FileWrite(StdOutPutHandle, Data.Rows[0], BytesPerRow * Size);
   {$endif}
   with Data do begin
-    //FreeMem(InitialIR);
+    FreeMem(InitialIR);
     FreeMem(Rows);
   end;
 end.
